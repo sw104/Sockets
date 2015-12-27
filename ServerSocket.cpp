bca2dd1d5aa2cb15a545fdcdd1eefa1685e62622
Sam <mail@example.com>
2015-12-27 22:47:57 +0000

    Formatting changes and bug fixes (now compiles).

--- ServerSocket.cpp
+++ ServerSocket.cpp
@@ -3,15 +3,15 @@
 
 //Private Methods:
 
-ServerSocket::create(int port)
+void ServerSocket::create(int port)
 {
     serverAddr.sin_port = htons(port);  //Set port number in a network byte order.
     serverAddr.sin_family = AF_INET;    //Set type TCP.
     serverAddr.sin_addr.s_addr = INADDR_ANY;    //Address of server.
-    setSocket(socket(AF_INET, SOCK_STREAM, 0));	    //Open socket.
+    setSocket(::socket(AF_INET, SOCK_STREAM, 0));	    //Open socket.
     if (getSocket() < 0)
         throw SocketException("Server Socket Creation: Error opening socket");
-    if (bind(getSocket(), (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0)    //Bind the socket to the sepcified port.
+    if (::bind(getSocket(), (struct sockaddr*) &serverAddr, sizeof(serverAddr)) < 0)    //Bind the socket to the sepcified port.
         throw SocketException("Error binding socket to port");
 }
 
@@ -26,10 +26,6 @@ ServerSocket::ServerSocket(int port)
     create(port);
 }
 
-ServerSocket::~ServerSocket()
-{
-}
-
 void ServerSocket::listen(int port)
 {
     create(port);
@@ -38,13 +34,13 @@ void ServerSocket::listen(int port)
 
 void ServerSocket::listen()
 {
-    listen(getSocket(), 5);	//Listen on socket, max. 5 queued connections.
+    ::listen(getSocket(), 5);	//Listen on socket, max. 5 queued connections.
     clientLen = sizeof(clientAddr);
 }
 
 int ServerSocket::accept()
 {
-    int clientfd = accept(getSocket(), (struct sockaddr*)&clientAddr, &clientLen);	//Accept client connection.
+    int clientfd = ::accept(getSocket(), (struct sockaddr*)&clientAddr, &clientLen);	//Accept client connection.
     if (clientfd < 0)
         throw SocketException("Server Socket Creation: Error accepting new client connection");
     return clientfd;
--- ServerSocket.h
+++ ServerSocket.h
@@ -2,13 +2,13 @@
 #ifndef SERVERSOCKET_H
 #define SERVERSOCKET_H
 
-#include "Socket.h"				//Generic socket properties.
+#include "Socket.h" //Generic socket properties.
 
     class ServerSocket: public Socket
     {
         struct sockaddr_in serverAddr, clientAddr;	//serverAddr contains the server address, and clientAddr a connected client address. 
         socklen_t clientLen;						//Contains the size of the address of the client.
-        void create(int port);      //Opens socket and binds to specified port.
+        void create(int port);                      //Opens socket and binds to specified port.
 
         public:
             ServerSocket();			//Default constructor (empty).
