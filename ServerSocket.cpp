e0b9ef1ee47f84e9e7c88e60d9456d4c9b42c892
Sam <mail@example.com>
2015-12-27 22:51:17 +0000

    ServerSocket class methods restructured.

--- ServerSocket.cpp
+++ ServerSocket.cpp
@@ -1,20 +1,6 @@
 //Server Socket Implementation.
 #include "ServerSocket.h"
 
-//Private Methods:
-
-void ServerSocket::create(int port)
-{
-    serverAddr.sin_port = htons(port);  //Set port number in a network byte order.
-    serverAddr.sin_family = AF_INET;    //Set type TCP.
-    serverAddr.sin_addr.s_addr = INADDR_ANY;    //Address of server.
-    setSocket(::socket(AF_INET, SOCK_STREAM, 0));	    //Open socket.
-    if (getSocket() < 0)
-        throw SocketException("Server Socket Creation: Error opening socket");
-    if (::bind(getSocket(), (struct sockaddr*) &serverAddr, sizeof(serverAddr)) < 0)    //Bind the socket to the sepcified port.
-        throw SocketException("Error binding socket to port");
-}
-
 //Public Methods:
 
 ServerSocket::ServerSocket()    //Default constructor.	
@@ -23,17 +9,19 @@ ServerSocket::ServerSocket()    //Default constructor.
 
 ServerSocket::ServerSocket(int port)
 {
-    create(port);
+    listen(port);
 }
 
 void ServerSocket::listen(int port)
-{
-    create(port);
-    listen();
-}
-
-void ServerSocket::listen()
-{
+{ 
+    serverAddr.sin_port = htons(port);  //Set port number in a network byte order.
+    serverAddr.sin_family = AF_INET;    //Set type TCP.
+    serverAddr.sin_addr.s_addr = INADDR_ANY;    //Address of server.
+    setSocket(::socket(AF_INET, SOCK_STREAM, 0));	    //Open socket.
+    if (getSocket() < 0)
+        throw SocketException("Server Socket Creation: Error opening socket");
+    if (::bind(getSocket(), (struct sockaddr*) &serverAddr, sizeof(serverAddr)) < 0)    //Bind the socket to the sepcified port.
+        throw SocketException("Error binding socket to port");
     ::listen(getSocket(), 5);	//Listen on socket, max. 5 queued connections.
     clientLen = sizeof(clientAddr);
 }
